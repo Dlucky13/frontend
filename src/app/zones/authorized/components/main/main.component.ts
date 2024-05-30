3a6c420f8ce2d3f95b436f9061377210961ca38d
dmitry.pashkurov <dmitry.pahkurov@mail.ru>
2024-05-30 19:19:12 +0300

    adding heading for authorized main component

--- src/app/zones/authorized/components/main/main.component.ts
+++ src/app/zones/authorized/components/main/main.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../../../../shared/services/auth.service';
-
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '@shared/services';
 @Component({
   selector: 'app-authorized-main',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatSidenavModule, FormsModule, MatIconModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+
 
   constructor(private authService: AuthService) { }
 
