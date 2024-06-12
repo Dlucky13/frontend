e6e92f04147fc1e1ed3c690d0043a1733aeaa4f4
dmitry.pashkurov <dmitry.pahkurov@mail.ru>
2024-06-13 00:28:57 +0300

    user-card add form and update styles

--- src/app/zones/authorized/components/user-card/user-card.component.ts
+++ src/app/zones/authorized/components/user-card/user-card.component.ts
@@ -1,24 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { AuthService, UserService } from '@shared/services';
+import { MatInputModule } from '@angular/material/input';
+import { AuthService, FormService, UserService } from '@shared/services';
 
 @Component({
   selector: 'app-user-card',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, ReactiveFormsModule, MatInputModule],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss'
 })
-export class UserCardComponent {
+export class UserCardComponent implements OnInit {
 
   editMode = false;
+  controlNames = ['name', 'role', 'birthdate'];
+  form!: FormGroup;
 
   constructor(
     private authService: AuthService,
-    private userService: UserService
+    private userService: UserService,
+    private formService: FormService
   ) { }
+
+  ngOnInit(): void {
+    this.form = this.formService.createForm(this.controlNames);
+  }
 
   editToggle(): void {
     this.editMode = !this.editMode;
